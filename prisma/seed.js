0e2919af4589b0b599520a84b5ef7991bca90af4
Andrew Carolan <acarolan@brainstation.io>
2022-08-04 17:22:42 -0400

    Add database seed.

---
+++ prisma/seed.js
@@ -0,0 +1,48 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const pinky = await prisma.user.upsert({
+    where: { email: "pinky@acme.co" },
+    update: {},
+    create: {
+      email: "pinky@acme.co",
+      name: "Pinky",
+      posts: {
+        create: {
+          title: "Narf!",
+        },
+      },
+    },
+  });
+
+  const brain = await prisma.user.upsert({
+    where: { email: "brain@acme.co" },
+    update: {},
+    create: {
+      email: "brain@acme.co",
+      name: "Brain",
+      posts: {
+        create: [
+          {
+            title: "My plan to take over the world",
+            content: "The same thing we do every night, Pinky...",
+            published: true,
+          },
+        ],
+      },
+    },
+  });
+  console.log({ pinky, brain });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
